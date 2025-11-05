a838b647bceddb1514be5710cfca40f1437d5a9b
Jonathan <jonathanjoel0510@gmail.com>
2025-11-04 21:06:18 -0500

    Delete IntroduccionPython directory

--- IntroduccionPython/Ejercicio #1.py
+++
@@ -1,7 +0,0 @@
-# Reto: Preséntate con Python
-# Mostrar el nombre completo
-print("Mi nombre es: Jonathan Bailon")
-# Mostrar la edad
-print("Tengo 25 años")
-# Mostrar el país de origen
-print("Soy de Ecuador")
--- IntroduccionPython/hola_mundo.py
+++
@@ -1,4 +0,0 @@
-# Primer programa de Hola Mundo con Python
-
-print("Hola mundo")
-print("Saludos...")
